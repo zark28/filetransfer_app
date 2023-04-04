34931862a7fa59686b77413a4228a3af113de914
zark28 <adamsabdulrazak28@gmail.com>
2023-04-04 00:36:38 +0000

    update server port

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const server = require("http").createServer(app);
 
 const io = require("socket.io")(server);
+const port = process.env.PORT || 5000
 
 app.use(express.static(path.join(__dirname+"/public")));
 
@@ -27,4 +28,4 @@ io.on("connection", function(socket){
 	})
 });
 
-server.listen(5000,()=>console.log("server running on port 5000"));
+server.listen(port,()=>console.log("server running on port 5000"));
